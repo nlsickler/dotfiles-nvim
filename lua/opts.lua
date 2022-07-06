115a642f4fd0a4f3592396185a70f0afe391ec0a
Nathan Sickler <nlsickler@sep.com>
2022-07-06 14:04:48 -0400

    Updating tabstop and shiftwidth

--- lua/opts.lua
+++ lua/opts.lua
@@ -13,8 +13,8 @@ vim.opt.linebreak = true
 
 -- Indentation
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 vim.opt.smartindent = true
 vim.opt.list = true
 vim.opt.listchars='tab:→ ,trail:•'
