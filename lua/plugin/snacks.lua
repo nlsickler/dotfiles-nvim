5e2f3ea8975c5b68b60cdf230ee4a78a5c658e3c
Nathan L. Sickler <nlsickler@sep.com>
2025-06-02 10:34:32 -0400

    Updated size of Projects section to extend for usage at work

--- lua/plugin/snacks.lua
+++ lua/plugin/snacks.lua
@@ -16,8 +16,8 @@ M.config = function()
           sections = {
             { section = "header" },
             { section = "keys", gap = 1, padding = 1 },
-            { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-            { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+            { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1, limit = 4 },
+            { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1, limit = 8 },
             {
               pane = 2,
               icon = " ",
